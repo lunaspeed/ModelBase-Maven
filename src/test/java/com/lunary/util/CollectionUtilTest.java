bc3f212e9fdbb4b5bcd548f4b8933c045f0cabbc
Steven Yang <kenshin520@gmail.com>
2012-11-01 12:01:59 +0800

    changed test cases, still have error

---
+++ src/test/java/com/lunary/util/CollectionUtilTest.java
@@ -0,0 +1,29 @@
+
+package com.lunary.util;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class CollectionUtilTest {
+
+  @Test
+  public void isEmpty() {
+    
+    assertTrue("null collection is empty", CollectionUtil.isEmpty(null));
+    assertTrue("empty List is empty", CollectionUtil.isEmpty(new ArrayList<Object>()));
+  }
+  
+  @Test
+  public void isNotEmpty() {
+    
+    List<Object> list = new ArrayList<Object>();
+    list.add(new Object());
+    assertFalse("collection with one element", CollectionUtil.isEmpty(list));
+    assertTrue("collection with one element", CollectionUtil.isEmpty(list));
+  }
+}
