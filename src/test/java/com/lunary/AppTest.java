fdc8ded6d1e78d751b10b683f15fa68ab319e8ca
Steven Yang <kenshin520@gmail.com>
2012-11-01 10:43:12 +0800

    remove some unused methods

--- src/test/java/com/lunary/AppTest.java
+++ src/test/java/com/lunary/AppTest.java
@@ -1,12 +1,8 @@
 package com.lunary;
 
-import java.util.Arrays;
-
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-
-import com.lunary.util.StringUtil;
 
 /**
  * Unit test for simple App.
@@ -36,9 +32,9 @@ public class AppTest  extends TestCase
      */
     public void testApp() {
       
-      String csv = "123,466,abc,,,,";
-      String[] values = StringUtil.split(csv, ',');
-      assertTrue(Arrays.equals(values, new String[] {"123", "466", "abc", "", "", "", ""}));
+//      String csv = "123,466,abc,,,,";
+//      String[] values = StringUtil.split(csv, ',');
+//      assertTrue(Arrays.equals(values, new String[] {"123", "466", "abc", "", "", "", ""}));
       
       assertTrue(false);
     }
