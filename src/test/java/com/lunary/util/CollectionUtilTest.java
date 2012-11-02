995c9a5520c1ac0c255cd5730d2f5700931948ad
Steven Yang <kenshin520@gmail.com>
2012-11-02 10:16:33 +0800

    changed test case

--- src/test/java/com/lunary/util/CollectionUtilTest.java
+++ src/test/java/com/lunary/util/CollectionUtilTest.java
@@ -7,8 +7,6 @@ import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.List;
 
-import junit.framework.Assert;
-
 import org.junit.Test;
 
 public class CollectionUtilTest {
@@ -18,19 +16,22 @@ public class CollectionUtilTest {
     
     assertTrue("null collection is empty", CollectionUtil.isEmpty(null));
     assertTrue("empty List is empty", CollectionUtil.isEmpty(new ArrayList<Object>()));
-  }
-  
-  @Test
-  public void isNotEmpty() {
-    
     List<Object> list = new ArrayList<Object>();
     list.add(new Object());
     assertFalse("collection with one element", CollectionUtil.isEmpty(list));
-    assertTrue("collection with one element", CollectionUtil.isEmpty(list));
   }
   
-  @Test
-  public void fail() {
-    Assert.fail("this is intended failure");
-  }
+//  @Test
+//  public void isNotEmpty() {
+//    
+//    List<Object> list = new ArrayList<Object>();
+//    list.add(new Object());
+//    assertFalse("collection with one element", CollectionUtil.isEmpty(list));
+//    assertTrue("collection with one element", CollectionUtil.isEmpty(list));
+//  }
+  
+//  @Test
+//  public void fail() {
+//    Assert.fail("this is intended failure");
+//  }
 }
